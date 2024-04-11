cbc0684bfd559a6f4029301d77e727cb945b9c70
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2024-04-11 01:08:58 -0300

    feat: add kafka publisher

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/bmviniciuss/sagas-golang
 go 1.22.0
 
 require (
+	github.com/confluentinc/confluent-kafka-go/v2 v2.3.0
 	github.com/google/uuid v1.6.0
 	github.com/stretchr/testify v1.9.0
 	go.uber.org/zap v1.27.0
---
+++ internal/streaming/publisher.go
@@ -0,0 +1,52 @@
+package streaming
+
+import (
+	"context"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"go.uber.org/zap"
+)
+
+type Publisher struct {
+	logger *zap.SugaredLogger
+	kfkCfg *kafka.ConfigMap
+}
+
+func NewPublisher(logger *zap.SugaredLogger, kfkCfg *kafka.ConfigMap) *Publisher {
+	return &Publisher{
+		logger: logger,
+		kfkCfg: kfkCfg,
+	}
+}
+
+func (p *Publisher) Publish(ctx context.Context, destination string, data []byte) error {
+	l := p.logger
+	l.Infof("Publishing message to destination %s", destination)
+
+	prod, err := kafka.NewProducer(p.kfkCfg)
+	if err != nil {
+		p.logger.With(zap.Error(err)).Error("Failed to create producer")
+		return err
+	}
+
+	deliveryChan := make(chan kafka.Event, 100)
+	defer close(deliveryChan)
+	err = prod.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &destination, Partition: kafka.PartitionAny},
+		Value:          data},
+		deliveryChan,
+	)
+	if err != nil {
+		p.logger.With(zap.Error(err)).Error("Failed to produce message")
+		return err
+	}
+
+	e := <-deliveryChan
+	m := e.(*kafka.Message)
+	if m.TopicPartition.Error != nil {
+		p.logger.Errorf("Delivery failed: %v\n", m.TopicPartition.Error)
+	} else {
+		p.logger.Infof("Delivered message to topic %s [%d] at offset %v\n",
+			*m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
+	}
+	return nil
+}
