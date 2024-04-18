5fdf296c7eb300d8a1df9cd10a6c7b07f91fb47e
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2024-04-17 21:01:39 -0300

    feat: add response pkg

---
+++ pkg/responses/error.go
@@ -0,0 +1,43 @@
+package responses
+
+import "net/http"
+
+type Error struct {
+	Status int         `json:"-"`
+	ID     string      `json:"id"`
+	Err    ErrorDetail `json:"error"`
+}
+
+type ErrorDetail struct {
+	Code    int          `json:"code"`
+	Message string       `json:"message"`
+	Details []FieldError `json:"details,omitempty"`
+}
+
+type FieldError struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+func NewInternalServerErrorResponse(id string) Error {
+	return Error{
+		ID:     id,
+		Status: http.StatusInternalServerError,
+		Err: ErrorDetail{
+			Code:    http.StatusInternalServerError,
+			Message: "Internal Server Error",
+		},
+	}
+}
+
+func NewBadRequestErrorResponse(id string, details []FieldError) Error {
+	return Error{
+		ID:     id,
+		Status: http.StatusBadRequest,
+		Err: ErrorDetail{
+			Code:    http.StatusBadRequest,
+			Message: "Bad Request",
+			Details: details,
+		},
+	}
+}
