0d96f214af62c1bb223c27aec01ab74aeb7692dc
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2024-05-06 23:13:54 -0300

    chore: add delete topics script

--- cmd/local/create-topics.sh
+++ cmd/local/create-topics.sh
@@ -1,6 +1,5 @@
 #/bin/bash
 
-docker exec broker bash /bin/kafka-topics --bootstrap-server localhost:9092 --list
 docker exec broker bash /bin/kafka-topics --bootstrap-server localhost:9092 --create --topic saga.create_order_v1.response --partitions 1 --replication-factor 1
 docker exec broker bash /bin/kafka-topics --bootstrap-server localhost:9092 --create --topic service.order.request --partitions 1 --replication-factor 1
 docker exec broker bash /bin/kafka-topics --bootstrap-server localhost:9092 --create --topic service.customer.request --partitions 1 --replication-factor 1
---
+++ cmd/local/delete-topics.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+docker exec broker bash /bin/kafka-topics --bootstrap-server localhost:9092 --delete --topic saga.create_order_v1.response
+docker exec broker bash /bin/kafka-topics --bootstrap-server localhost:9092 --delete --topic service.order.request
+docker exec broker bash /bin/kafka-topics --bootstrap-server localhost:9092 --delete --topic service.customer.request
+docker exec broker bash /bin/kafka-topics --bootstrap-server localhost:9092 --list
